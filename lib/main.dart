ea6341b4cd7fbb70bf76f8764e4f4f59aa8c4eba
Zahra Turabee <zahraturabee123@gmail.com>
2025-04-11 23:07:03 +0500

    created a new project gtalk

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen(), // Dashboard ya splash screen yahan set hoti hai
+      home: SplashScreen(),
     );
   }
 }
